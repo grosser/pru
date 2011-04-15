079e931c6fb6ed9079116888861c724d7f987704
grosser <grosser.michael@gmail.com>
2011-04-15 19:09:18 +0200

    first working version

--- lib/rup.rb
+++ lib/rup.rb
@@ -1,3 +1,32 @@
 class Rup
   VERSION = File.read( File.join(File.dirname(__FILE__),'..','VERSION') ).strip
 end
+
+class Object
+  unless defined? instance_exec # 1.9
+    module InstanceExecMethods #:nodoc:
+    end
+    include InstanceExecMethods
+
+    # Evaluate the block with the given arguments within the context of
+    # this object, so self is set to the method receiver.
+    #
+    # From Mauricio's http://eigenclass.org/hiki/bounded+space+instance_exec
+    def instance_exec(*args, &block)
+      begin
+        old_critical, Thread.critical = Thread.critical, true
+        n = 0
+        n += 1 while respond_to?(method_name = "__instance_exec#{n}")
+        InstanceExecMethods.module_eval { define_method(method_name, &block) }
+      ensure
+        Thread.critical = old_critical
+      end
+
+      begin
+        send(method_name, *args)
+      ensure
+        InstanceExecMethods.module_eval { remove_method(method_name) } rescue nil
+      end
+    end
+  end
+end
--- spec/rup_spec.rb
+++ spec/rup_spec.rb
@@ -4,4 +4,20 @@ describe Rup do
   it "has a VERSION" do
     Rup::VERSION.should =~ /^\d+\.\d+\.\d+$/
   end
+
+  it "maps" do
+    `ls -l | ./bin/rup 'include?("G")'`.split("\n").size.should == 2
+  end
+
+  it "maps and reduces" do
+    `ls -l | ./bin/rup 'include?("G")' 'size'`.should == "2\n"
+  end
+
+  it "maps with empty string and reduces" do
+    `ls -l | ./bin/rup '' 'size'`.should == "10\n"
+  end
+
+  it "reduces" do
+    `ls -l | ./bin/rup -r 'size'`.should == "10\n"
+  end
 end
