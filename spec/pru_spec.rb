5700c809036b8a847ff19a2e2c0920cc6696cae4
grosser <grosser.michael@gmail.com>
2011-04-18 18:41:38 +0200

    spec default reduce behavior

--- spec/pru_spec.rb
+++ spec/pru_spec.rb
@@ -1,10 +1,6 @@
 require File.expand_path('spec/spec_helper')
 
 describe Pru do
-  before :all do
-    @default = `ls -l | wc -l`
-  end
-
   it "has a VERSION" do
     Pru::VERSION.should =~ /^\d+\.\d+\.\d+$/
   end
@@ -18,7 +14,7 @@ describe Pru do
   end
 
   it "can selects via i" do
-    `ls -l | ./bin/pru 'i'`.split("\n")[0...3].should == ["1","2","3"]
+    `cat spec/test.txt | ./bin/pru 'i'`.split("\n")[0...3].should == ["1","2","3"]
   end
 
   it "maps" do
@@ -26,22 +22,26 @@ describe Pru do
   end
 
   it "selects and reduces" do
-    `ls -l | ./bin/pru 'include?("G")' 'size'`.should == "2\n"
+    `cat spec/test.txt | ./bin/pru 'include?("abc")' 'size'`.should == "2\n"
   end
 
   it "selects with empty string and reduces" do
-    `ls -l | ./bin/pru '' 'size'`.should == @default
+    `cat spec/test.txt | ./bin/pru '' 'size'`.should == "5\n"
   end
 
   it "reduces" do
-    `ls -l | ./bin/pru -r 'size'`.should == @default
+    `cat spec/test.txt | ./bin/pru -r 'size'`.should == "5\n"
+  end
+
+  it "prints arrays as newlines" do
+    `cat spec/test.txt | ./bin/pru -r 'self'`.should == File.read('spec/test.txt')
   end
 
   it "can sum" do
-    `echo 5 | ./bin/pru -r 'sum(&:to_i)'`.should == "5\n"
+    `cat spec/test.txt | ./bin/pru -r 'sum(&:to_i)'`.should == "1212\n"
   end
 
   it "can mean" do
-    `echo 5 | ./bin/pru -r 'mean(&:to_i)'`.should == "5.0\n"
+    `cat spec/test.txt | ./bin/pru -r 'mean(&:to_i)'`.should == "242.4\n"
   end
 end
