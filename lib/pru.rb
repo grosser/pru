eb8455b5cdf3eadc7d35f510916667821adc5fe7
nashby <younash@gmail.com>
2011-04-21 20:19:08 +0300

    little refactoring

--- lib/pru.rb
+++ lib/pru.rb
@@ -1,3 +1,5 @@
+require 'core_ext'
+
 class Pru
   VERSION = File.read( File.join(File.dirname(__FILE__),'..','VERSION') ).strip
 
@@ -8,9 +10,8 @@ class Pru
       end
     RUBY
 
-    i = 1
-    io.readlines.each do |line|
-      result = line[0..-2]._pru(i)
+    io.readlines.each_with_index do |line, i|
+      result = line[0..-2]._pru(i+1)
       if result == true
         yield line
       elsif result.is_a?(Regexp)
@@ -18,7 +19,6 @@ class Pru
       elsif result
         yield result
       end
-      i += 1
     end
   end
 
@@ -29,31 +29,5 @@ class Pru
       end
     RUBY
     array._pru
-  end
-end
-
-# http://madeofcode.com/posts/74-ruby-core-extension-array-sum
-class Array
-  def sum(method = nil, &block)
-    if block_given?
-      raise ArgumentError, "You cannot pass a block and a method!" if method
-      inject(0) { |sum, i| sum + yield(i) }
-    elsif method
-      inject(0) { |sum, i| sum + i.send(method) }
-    else
-      inject(0) { |sum, i| sum + i }
-    end
-  end
-end
-
-class Array
-  def mean(method = nil, &block)
-    sum(method, &block) / size.to_f
-  end
-end
-
-class Array
-  def grouped
-    group_by{|x|x}
   end
 end
